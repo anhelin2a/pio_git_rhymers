e22be4fa451fb3e426f1ef2e9ac75e010f211d41
miraslauuu <248655@edu.p.lodz.pl>
2024-05-23 11:55:12 +0200

    removing unused setters

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -15,26 +15,19 @@ public Node(int value) {
 
 
 
-    public void setValue(int value) {
-        this.value = value;
-    }
+ 
 
     public Node getPrev() {
         return prev;
     }
 
-    public void setPrev(Node prev) { 
-        this.prev = prev;
-    }
+
 
     public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
-    
+
         public Node(int i) {
         value = i;
     }
